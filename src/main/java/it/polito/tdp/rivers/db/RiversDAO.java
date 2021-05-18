9e52b9a152c05f87f3151d5b6d65fa0c0006b9c2
Lara <s258856@studenti.polito.it>
2021-05-18 13:00:26 +0200

    risoluzione punto 1

--- src/main/java/it/polito/tdp/rivers/db/RiversDAO.java
+++ src/main/java/it/polito/tdp/rivers/db/RiversDAO.java
@@ -2,7 +2,10 @@ package it.polito.tdp.rivers.db;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
+import it.polito.tdp.rivers.model.Flow;
+import it.polito.tdp.rivers.model.Info;
 import it.polito.tdp.rivers.model.River;
 
 import java.sql.Connection;
@@ -35,5 +38,63 @@ public class RiversDAO {
 		}
 
 		return rivers;
+	}
+	
+	public Info getInfoRiver(int id) {
+		String sql = "SELECT MAX(f.day) AS max, MIN(f.day) AS min, AVG(f.flow) AS media, COUNT(*) AS tot "
+				+ "FROM flow f "
+				+ "WHERE f.river = ?";
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet res = st.executeQuery();
+			res.next();
+			
+			Info i = new Info(res.getDate("min").toLocalDate(), res.getDate("max").toLocalDate(), res.getFloat("tot"), res.getFloat("media"));
+			
+			res.close();
+			st.close();
+			conn.close();
+			
+			return i;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("SQL Error", e);
+		}
+
+	}
+	
+	public River getRiver(int id) {
+		for(River r : this.getAllRivers()) {
+			if(r.getId() == id) {
+				return r;
+			}
+		}
+		return null;
+	}
+	
+	public void getAllFlows(PriorityQueue<Flow> coda, int id) {
+		String sql = "SELECT * "
+				+ "FROM flow "
+				+ "WHERE river = ?";
+		
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, id);
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				Flow flow = new Flow(res.getDate("day").toLocalDate(), res.getFloat("flow"), this.getRiver(res.getInt("river")));
+				coda.add(flow);
+			}
+
+			conn.close();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("SQL Error");
+		}
 	}
 }
